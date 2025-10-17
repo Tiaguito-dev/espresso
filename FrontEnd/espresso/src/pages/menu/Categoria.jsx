4ca216d73d19e279edfb8c9b69f6da630252df37
Avril Lugo Gonzalez <avriluchigonzalez@gmail.com>
2025-10-17 18:32:21 -0300

    -

--- FrontEnd/espresso/src/pages/menu/Categoria.jsx
+++ FrontEnd/espresso/src/pages/menu/Categoria.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import "../menu/ClienteMenu.css"
 
 export default function Categoria({ nombre, onClick, categoriaActiva }) {
-  // Asegúrate de usar tu clase CSS original 'btn-categoria'
   const claseBoton = `btn-categoria ${categoriaActiva ? 'activa' : ''}`;
 
   return (
