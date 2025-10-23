a74dda94150065dc6e26af2c89299114debd9c18
tiago-dev <tiaguitosolis@gmail.com>
2025-10-23 10:06:35 -0300

    algunos cambios

--- BackEnd/Test/TestPedidoBD.js
+++ BackEnd/Test/TestPedidoBD.js
@@ -1,45 +1,41 @@
 const PedidoBD = require('../repositories/PedidoBD');
 
-exports.runTests = () => {
+exports.runTests = async () => {
 
-    PedidoBD.obtenerPedidosHoy()
-        .then(pedidosData => {
-            console.log('Pedidos de hoy:', pedidosData);
-        })
-        .catch(error => {
-            console.error('Error al obtener los pedidos de hoy:', error);
-        });
+    try {
+        const pedidosHoy = await PedidoBD.obtenerPedidosHoy();
+        console.log('Pedidos de hoy:', pedidosHoy);
+    } catch (error) {
+        console.error('Error al obtener los pedidos de hoy:', error);
+    }
 
-    PedidoBD.obtenerPedidosFecha('2025-10-23')
-        .then(pedidosData => {
-            console.log('Pedidos de 2025-10-23:', pedidosData);
-        })
-        .catch(error => {
-            console.error('Error al obtener los pedidos de 2025-10-23:', error);
-        });
+    try {
+        const pedidosFecha = await PedidoBD.obtenerPedidosFecha('2025-10-23');
+        console.log('Pedidos de 2025-10-23:', pedidosFecha);
+    } catch (error) {
+        console.error('Error al obtener los pedidos de 2025-10-23:', error);
+    }
 
-    PedidoBD.obtenerUltimoNroPedido()
-        .then(ultimoNro => {
-            console.log('Último número de pedido:', ultimoNro);
-        })
-        .catch(error => {
-            console.error('Error al obtener el último número de pedido:', error);
-        });
+    try {
+        const ultimoNro = await PedidoBD.obtenerUltimoNroPedido();
+        console.log('Último número de pedido:', ultimoNro);
+    } catch (error) {
+        console.error('Error al obtener el último número de pedido:', error);
+    }
 
-    PedidoBD.modificarEstadoPedido(1, 'Pagado')
-        .then(resultado => {
-            console.log(resultado.message);
-        })
-        .catch(error => {
-            console.error('Error al obtener el último número de pedido:', error);
-        });
-    PedidoBD.obtenerPedidoPorNro(1)
-        .then(pedido => {
-            console.log('Pedido nro 1:', pedido);
-        })
-        .catch(error => {
-            console.error('Error al obtener el pedido nro 1:', error);
-        });
+    try {
+        const resultado = await PedidoBD.modificarEstadoPedido(1, 'Pagado');
+        console.log(resultado.message);
+    } catch (error) {
+        console.error('Error al modificar el estado del pedido:', error);
+    }
+
+    try {
+        const pedido = await PedidoBD.obtenerPedidoPorNro(1);
+        console.log('Pedido nro 1:', pedido);
+    } catch (error) {
+        console.error('Error al obtener el pedido nro 1:', error);
+    }
 
     /*
     PedidoBD.crearPedido({
