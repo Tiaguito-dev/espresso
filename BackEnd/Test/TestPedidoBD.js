a3bd954b75dd7c71ecc0b9c740bfc90959a66d59
tiago-dev <tiaguitosolis@gmail.com>
2025-10-23 09:30:50 -0300

    PedidosBD estaría completo

---
+++ BackEnd/Test/TestPedidoBD.js
@@ -0,0 +1,60 @@
+const PedidoBD = require('../repositories/PedidoBD');
+
+exports.runTests = () => {
+
+    PedidoBD.obtenerPedidosHoy()
+        .then(pedidosData => {
+            console.log('Pedidos de hoy:', pedidosData);
+        })
+        .catch(error => {
+            console.error('Error al obtener los pedidos de hoy:', error);
+        });
+
+    PedidoBD.obtenerPedidosFecha('2025-10-23')
+        .then(pedidosData => {
+            console.log('Pedidos de 2025-10-23:', pedidosData);
+        })
+        .catch(error => {
+            console.error('Error al obtener los pedidos de 2025-10-23:', error);
+        });
+
+    PedidoBD.obtenerUltimoNroPedido()
+        .then(ultimoNro => {
+            console.log('Último número de pedido:', ultimoNro);
+        })
+        .catch(error => {
+            console.error('Error al obtener el último número de pedido:', error);
+        });
+
+    PedidoBD.modificarEstadoPedido(1, 'Pagado')
+        .then(resultado => {
+            console.log(resultado.message);
+        })
+        .catch(error => {
+            console.error('Error al obtener el último número de pedido:', error);
+        });
+    PedidoBD.obtenerPedidoPorNro(1)
+        .then(pedido => {
+            console.log('Pedido nro 1:', pedido);
+        })
+        .catch(error => {
+            console.error('Error al obtener el pedido nro 1:', error);
+        });
+
+    /*
+    PedidoBD.crearPedido({
+        nroPedido: 5,
+        fecha: new Date(),
+        observacion: 'Pedido de prueba',
+        monto: 100,
+        idMozo: 1,
+        idMesa: 1
+    })
+    .then(resultado => {
+        console.log('Pedido creado:', JSON.stringify(resultado));
+    })
+    .catch(error => {
+        console.error('Error al crear el pedido:', error);
+    });
+    */
+}
