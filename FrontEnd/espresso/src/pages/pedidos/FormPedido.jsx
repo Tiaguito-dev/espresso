78c16e02cf43092bd290c48a08abf9fc4bf16927
Williams-Ignacio <ignacio.williams@alu.frlp.utn.edu.ar>
2025-11-09 20:08:13 -0300

    primer avance del modificar pedido

--- FrontEnd/espresso/src/pages/pedidos/FormPedido.jsx
+++ FrontEnd/espresso/src/pages/pedidos/FormPedido.jsx
@@ -1,14 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { createPedido, updatePedido, buscarPedidoPorId } from "../../services/pedidosService"; 
+import { updatePedido, buscarPedidoPorId } from "../../services/pedidosService"; 
 import "./AgregarPedido.css";
 
 const FormPedido = () => {
+
+    const [mostrarFiltros, setMostrarFiltros] = useState(false);
+    const [estadoFiltro, setEstadoFiltro] = useState("todos");
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [pedido, setPedido] = useState({
+        id: id,
+        fecha: "",
+        estadoPedido: "",
+        mesa: {
+            nroMesa:"",
+            estadoMesa:""
+        },
+        lineasPedido: [
+            {
+                idProducto: "",
+                cantidad: "",
+                nombreProducto: "",
+                precioUnitario: ""
+            }
+        ]
+        
+    });
+
+    useEffect(() => {
+        fetchPedido();
+    }, []);
+
+    const fetchPedido = async () => {
+        try {
+            const data = await buscarPedidoPorId(id);
+            setPedido(data);
+            console.log(data);
+        } catch (error) {
+            console.error("Error no se pudo obtener el pedido:", error);
+        }
+    };
+
     return (
         <div>
-            <h1>prueba</h1>
+            prueba
         </div>
-
     )
 }
 
